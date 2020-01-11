ed578d527d324b269523b046eafae19ec52bb03a
Rintu Gochhayat <rintuweb4@gmail.com>
2020-01-11 12:15:51 +0530

    Updates

---
+++ precache-manifest.12f2db401f43ee4d53043f199fed5cdd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8efb69cf072e2fc1a8bff79bad2647",
+    "url": "/Scrolling-React-App/index.html"
+  },
+  {
+    "revision": "7349a25cbb31803fc9b3",
+    "url": "/Scrolling-React-App/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bd527b98a041834ac1a4",
+    "url": "/Scrolling-React-App/static/css/main.1377542c.chunk.css"
+  },
+  {
+    "revision": "7349a25cbb31803fc9b3",
+    "url": "/Scrolling-React-App/static/js/2.70183818.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/Scrolling-React-App/static/js/2.70183818.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd527b98a041834ac1a4",
+    "url": "/Scrolling-React-App/static/js/main.c68e52cf.chunk.js"
+  },
+  {
+    "revision": "ab0b6f18cb66b3e01bb4",
+    "url": "/Scrolling-React-App/static/js/runtime-main.064ac272.js"
+  },
+  {
+    "revision": "56da9f99e17347d58cddd5f68c59b95e",
+    "url": "/Scrolling-React-App/static/media/aboutImage.56da9f99.png"
+  },
+  {
+    "revision": "f5bfbf686073bfbf38f3a37be3895c3a",
+    "url": "/Scrolling-React-App/static/media/applePlaystore.f5bfbf68.png"
+  },
+  {
+    "revision": "963a548079a89c61731401ac3c3ae8aa",
+    "url": "/Scrolling-React-App/static/media/banner.963a5480.png"
+  },
+  {
+    "revision": "9318636e11ff205c29a2df8ffc560f81",
+    "url": "/Scrolling-React-App/static/media/bg-1.9318636e.jpg"
+  },
+  {
+    "revision": "5a39305bffcbb6ebf26f361ada01a4bc",
+    "url": "/Scrolling-React-App/static/media/googlePlaystore.5a39305b.png"
+  },
+  {
+    "revision": "fa18c8159977039522431d3c8ced2748",
+    "url": "/Scrolling-React-App/static/media/picT1.fa18c815.png"
+  },
+  {
+    "revision": "3f15c2f43bd564a64c373d387d8ebd72",
+    "url": "/Scrolling-React-App/static/media/picT2.3f15c2f4.png"
+  },
+  {
+    "revision": "66779ecc42a5b7668085bfe60ea9ab9c",
+    "url": "/Scrolling-React-App/static/media/picT3.66779ecc.png"
+  }
+]);
